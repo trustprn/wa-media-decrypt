7d33ea942c0487b168f654560d340b72f1b15a15
trustprn <galuhdpranoto@student.untan.ac.id>
2025-08-03 03:15:41 +0700

    fix: update decrypt.js

--- api/decrypt.js
+++ api/decrypt.js
@@ -7,10 +7,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { url, mediaKey, mimetype } = req.body;
-
-    const encrypted = (await axios.get(url, { responseType: "arraybuffer" }))
-      .data;
+    const { url, mediaKey, mimetype, directPath, fileLength } = req.body;
 
     const decrypted = await downloadMediaMessage(
       {
@@ -24,11 +21,11 @@ export default async function handler(req, res) {
             url,
             mimetype,
             mediaKey: Buffer.from(mediaKey, "base64"),
-            fileEncSha256: Buffer.alloc(32),
-            fileSha256: Buffer.alloc(32),
+            fileEncSha256: Buffer.alloc(32), // optional
+            fileSha256: Buffer.alloc(32), // optional
             mediaKeyTimestamp: 0,
-            directPath: "",
-            fileLength: "0",
+            directPath, // ✅ isi sesuai body
+            fileLength: Number(fileLength), // ✅ pastikan ini number
           },
         },
       },
@@ -39,7 +36,7 @@ export default async function handler(req, res) {
     res.setHeader("Content-Type", mimetype);
     return res.status(200).send(decrypted);
   } catch (err) {
-    console.error(err);
+    console.error("DECRYPT ERROR:", err);
     return res.status(500).json({ error: "Failed to decrypt" });
   }
 }
